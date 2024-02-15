b2396476f662106346a0f4bd26a4851590850775
Sebatian <Sxe1exs@gmail.com>
2024-02-15 12:42:38 -0700

    Initalize

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+const Header = ({}) => {
+    return(
+        <div className="page-header">
+            <div className="logo">
+                <span className="logo-letters">S</span><span className="small-logo-letters">x</span><span className="logo-letters">E</span>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import "./HomePage.css"
+
+const HomePage = ({}) => {
+    return(
+        <div className="page">
+
+        </div>
+    )
+}
+
+export default HomePage

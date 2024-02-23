c81c18344af8c17c6617bcd6e6a104aa0eaa371d
Sebatian <Sxe1exs@gmail.com>
2024-02-23 10:07:20 -0700

    Added an image path for slides

--- src/Components/Slide/Slide.jsx
+++ src/Components/Slide/Slide.jsx
@@ -1,6 +1,8 @@
 import './Slide.css'
 
-const Slide = ({index, darkMode, text, setSlidePostion, lastSlide}) => {
+import image from '../../Images/ViewDotaHomepage.png'
+
+const Slide = ({index, darkMode, text, setSlidePostion, lastSlide, imgPath}) => {
     console.log(index, text)
     const nextSlide = () => {
         setSlidePostion(prevSlide => prevSlide + 1)
@@ -21,7 +23,9 @@ const Slide = ({index, darkMode, text, setSlidePostion, lastSlide}) => {
     else if (lastSlide === true){
         return(
             <div className={`slide ${darkMode ? 'dark-background' : 'light-background'}`}>
-                <p className="description">{text}</p>
+                {imgPath &&
+                    <img className='slide-image' src={imgPath} alt="" />}
+                <p className="slide-text">{text}</p>
                 <button className={`arrow ${darkMode ? 'left-light-arrow' : 'left-dark-arrow'}`} onClick={() => prevSlide()}></button>
             </div>
         )
@@ -29,10 +33,14 @@ const Slide = ({index, darkMode, text, setSlidePostion, lastSlide}) => {
     else {
         return ( 
             <div className={`slide ${darkMode ? 'dark-background' : 'light-background'}`}>
-                <p className="description">{text}</p>
                 <div>
-                    <button className={`arrow ${darkMode ? 'left-light-arrow' : 'left-dark-arrow'}`} onClick={() => prevSlide()}></button>
-                    <button className={`arrow ${darkMode ? 'right-light-arrow' : 'right-dark-arrow'}` } onClick={() => nextSlide()}></button>
+                    {imgPath &&
+                        <img className='slide-image' src={imgPath} alt="" />}
+                    <p className="slide-text">{text}</p>
+                    <div className='slide-button-container'>
+                        <button className={`arrow ${darkMode ? 'left-light-arrow' : 'left-dark-arrow'}`} onClick={() => prevSlide()}></button>
+                        <button className={`arrow ${darkMode ? 'right-light-arrow' : 'right-dark-arrow'}` } onClick={() => nextSlide()}></button>
+                    </div>
                 </div>
             </div>
         );
--- src/Pages/ProjectsPage/ProjectsPage.jsx
+++ src/Pages/ProjectsPage/ProjectsPage.jsx
@@ -3,6 +3,9 @@ import ProjectDisplay from "../../Components/ProjectDisplays/ProjectDisplay";
 import "./ProjectsPage.css"
 
 import ViewDotaHomePage from "../../Images/ViewDotaHomepage.png"
+import ViewDotaAccountPage from "../../Images/ViewDotaAccountPage.png"
+import ViewDotaMatchHistoryPage from "../../Images/ViewDotaMatchHistoryPage.png"
+import ViewDotaMatchDetailsPage from "../../Images/ViewDotaMatchDetailsPage.png"
 import ConsoleBattleship from "../../Images/ConsoleBattleship.png"
 import VolumeVisualizer from "../../Images/VolumeVisualizer.png"
 
@@ -13,25 +16,29 @@ const ProjectsPage = () => {
     const { darkMode } = useContext(DarkModeContext)
 
     const aboutViewDota = 'An application that allow players of the game DOTA 2 to login with their Steam Id to see the statistics of the games they have played.'
+    const viewDotaSlide1 = 'Users account page where they are able to see the statistics from the games played within the selected time frame.'
+    const viewDotaSlide2 = 'The history of up to 100 previously played matches that can be sorted by each statistic. Each match can be selected to see more information.'
+    const viewDotaSlide3 = 'A more detailed look at an individual match, including the stats of any party members in the match. Comments are able to be left on the match that party members are also able to see.'
     const aboutVolumeVisualizer = 'Do you know what 50000 mililiters of water would look like? Not many people are able to easily visualize a volume just by the metric, so this application converts it into a form that is much easier to understand as well as displaying what it would actually look like!'
     const aboutConsoleBattleship = 'Programming games can be a great way to learn new technologies and push your abilites. I developed this console game as a test of my abilities in C# and to teach myself how to use arrow keys to control a program.'
 
     const viewDotaSlides = {
-        count: 3, 
-        about: aboutViewDota, 
-        slideText: ['this','something', 'another thing']
+        count: 4, 
+        slideText: [aboutViewDota, viewDotaSlide1, viewDotaSlide2, viewDotaSlide3],
+        slideImages: [null, ViewDotaAccountPage, ViewDotaMatchHistoryPage, ViewDotaMatchDetailsPage]
     }
 
     const volumeVisualizerSlides = {
         count: 3, 
-        about: aboutViewDota, 
-        slideText: [aboutViewDota,'something', 'another thing']
+        slideText: [aboutVolumeVisualizer,'something', 'another thing'], 
+        slideImages: [null, null, null]
     }
 
+
     const consoleBattleshipSlides = {
-        count: 3, 
-        about: aboutConsoleBattleship, 
-        slideText: [toString(aboutViewDota),'something', 'another thing']
+        count: 2, 
+        slideText: [aboutConsoleBattleship, ''], 
+        slideImages: [null, null]
     }
 
     return ( 
